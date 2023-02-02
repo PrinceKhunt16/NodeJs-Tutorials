436d65f75594080ec8b54c35ae3fd2da38fe94bd
PrinceKhunt16 <princekhuntdatastructure@gmail.com>
2023-02-02 10:28:25 +0530

    Path module in explore more functions

--- NodeJs/pathModule/index.js
+++ NodeJs/pathModule/index.js
@@ -1,7 +1,21 @@
-const path = require('path');
+const path = require('node:path')
 
-console.log(path.basename(__filename));
-console.log(path.dirname(__filename));
-console.log(path.extname(__filename));
-console.log(path.parse(__filename));
-console.log(path.join(__dirname, 'app.js'));
+console.log(__filename)
+console.log(__dirname)
+
+console.log(path.basename(__filename))
+console.log(path.basename(__dirname))
+
+console.log(path.extname(__filename))
+console.log(path.extname(__dirname))
+
+console.log(path.dirname(__filename))
+console.log(path.dirname(__dirname))
+
+console.log(path.parse(__filename))
+
+console.log(path.isAbsolute(__filename))
+
+console.log(path.join(__dirname, 'app.js'))
+
+console.log(path.resolve(__dirname, 'app.js'))
