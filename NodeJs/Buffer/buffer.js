e9e9564f1b53195cf50990ff04085b8da27699ba
PrinceKhunt16 <princekhuntdatastructure@gmail.com>
2023-02-03 09:46:22 +0530

    Buffer

---
+++ NodeJs/Buffer/buffer.js
@@ -0,0 +1,5 @@
+const buffer = new Buffer.from('John')
+
+console.log(buffer) 
+console.log(buffer.toJSON())
+console.log(buffer.toString())
--- NodeJs/eventsModule/example.js
+++ NodeJs/eventsModule/example.js
@@ -1,13 +1,13 @@
-const EventEmitter = require('events');
+const EventEmitter = require('node:events')
 
-const customEmitter = new EventEmitter();
+const customEmitter = new EventEmitter()
 
 customEmitter.on('response', (name, id) => {
-    console.log(`data recieved by ${name} and his id is ${id}`);
-});
+    console.log(`data recieved by ${name} and his id is ${id}`)
+})
 
 customEmitter.on('response', () => {
-    console.log(`data passed`);
-});
+    console.log(`data passed`)
+})
 
-customEmitter.emit('response', 'john', 34);
+customEmitter.emit('response', 'john', 34)
